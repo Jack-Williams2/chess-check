5766418faaf8e1d0f5d91824b5e8df7efc0e5783
Jack <s3788167@student.rmit.edu.au>
2023-02-03 12:45:01 +1100

    fixing async issue

--- chess-app/src/App.test.js
+++ chess-app/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', async () => {
-  await render(<App />);
+  render(<App />);
   const linkElement = screen.getByText('Chess App');
   await expect(linkElement).toBeInTheDocument();
 });
